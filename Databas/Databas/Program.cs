8ec3aa32f7835a3d11cf49bd36651c6b65ab518b
Eftiand <kristianjer@gmail.com>
2017-03-02 16:38:01 +0100

    Add files via upload

---
+++ Databas/Databas/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using User;
+
+public class Program
+{
+    UserConnect currUser;
+    static void Main()
+    {
+        Console.WriteLine("1.Log In\n2.Create new user");
+        int val = ReadInt();
+        switch (val)
+        {
+            case 1:
+                break;
+            case 2:
+                Console.Write("UserName:");
+                string user = ReadString();
+                Console.Write("Password:");
+                string pass = ReadString();
+                try
+                {
+                    UserConnect connect = new UserConnect(1,user, pass);
+                    connect.CreateNewUser(user,pass);
+                    Console.WriteLine("Succefully created user");
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    
+                }
+                break;
+            default:
+                Console.WriteLine("Mellan 1-2s");
+                break;
+        }
+        Console.ReadLine();
+
+    }
+    static int ReadInt()
+    {
+        while (true)
+        {
+            int readInteger;
+
+            while (!int.TryParse(Console.ReadLine(), out readInteger))
+                Console.WriteLine("Endast siffror");
+
+            if (readInteger < 0)
+                Console.WriteLine("Endast positiva tal");
+            
+            else
+                return readInteger;
+        }
+    }
+    static string ReadString()
+    {
+        string temp="";
+
+        while (string.IsNullOrEmpty(temp)||temp.Any(ch=>!Char.IsLetterOrDigit(ch)))
+        {
+            temp = Console.ReadLine();
+        }
+        return temp;
+    }
+}
---
+++ Databas/Databas/UserConnect.cs
@@ -0,0 +1,63 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User
+{
+    public class UserConnect
+    {
+        MySqlConnection dbConn;
+        public String SERVER = "127.0.0.1";
+        public String DATABASE = "logins";
+        public String UID = "root";
+        public String PASSWORD = "1234";
+
+        public int Id { get; private set; }
+        public string UserName { get; private set; }
+        public string Password { get; private set; }
+
+
+        public UserConnect(int id, string u, string p)
+        {
+            StartDataBase();
+            Id = id;
+            UserName = u;
+            Password = p;
+        }
+        void StartDataBase()
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder();
+            builder.Server = SERVER;
+            builder.UserID = UID;
+            builder.Password = PASSWORD;
+            builder.Database = DATABASE;
+
+            string connString = builder.ToString();
+            builder = null;
+            dbConn = new MySqlConnection(connString);
+        }
+        void Login()
+        {
+
+        }
+        public void CreateNewUser(string u, string p)
+        {
+            Console.WriteLine("Connecting server...");
+            string query = string.Format("INSERT INTO users(id, username, password) VALUES (@i,@Us,@Ps)");
+            MySqlCommand cmd = new MySqlCommand(query, dbConn);
+            dbConn.Open();
+            int id = (int)cmd.LastInsertedId;
+
+            cmd.Parameters.AddWithValue("@i", id);
+            cmd.Parameters.AddWithValue("@Us", u);
+            cmd.Parameters.AddWithValue("@Ps", p);
+            cmd.ExecuteNonQuery();
+            
+
+            dbConn.Close();
+        }
+    }
+}
